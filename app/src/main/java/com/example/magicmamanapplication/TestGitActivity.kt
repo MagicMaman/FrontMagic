7b8ba291aabf7702a1942fca3809e325427287bf
bensalemchaima <chaima.bensalem@esprit.tn>
2021-12-25 21:24:10 +0100

    Test Git

---
+++ app/src/main/java/com/example/magicmamanapplication/TestGitActivity.kt
@@ -0,0 +1,11 @@
+package com.example.magicmamanapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TestGitActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_git)
+    }
+}

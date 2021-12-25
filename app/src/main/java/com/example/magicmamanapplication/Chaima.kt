475fa2fd2b3b16d0099c7353782221e9d41a83f8
benothmenwalid <walid.benothmen@esprit.tn>
2021-12-26 00:18:07 +0100

    AddChaima

---
+++ app/src/main/java/com/example/magicmamanapplication/Chaima.kt
@@ -0,0 +1,11 @@
+package com.example.magicmamanapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Chaima : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chaima)
+    }
+}

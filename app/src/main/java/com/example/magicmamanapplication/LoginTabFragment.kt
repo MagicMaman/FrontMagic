6910d2048c76675341b0d45be91b117a2c9c15f8
benothmenwalid <walid.benothmen@esprit.tn>
2021-12-26 22:14:45 +0100

    updateWalid

--- app/src/main/java/com/example/magicmamanapplication/LoginTabFragment.kt
+++ app/src/main/java/com/example/magicmamanapplication/LoginTabFragment.kt
@@ -31,7 +31,9 @@ class LoginTabFragment : Fragment() {
         bind.imageView.setOnClickListener {
           /*  val intent= Intent(this@LoginTabFragment.requireContext(), Identify::class.java)
             startActivity(intent)*/
+
                 doLogin()
+
 
         }
         return bind.root

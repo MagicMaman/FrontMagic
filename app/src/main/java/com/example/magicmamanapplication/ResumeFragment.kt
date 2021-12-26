b6332f77101c1269457f71b49e41398bdf8fb07b
bensalemchaima <chaima.bensalem@esprit.tn>
2021-12-26 19:33:06 +0100

    UpdateChaima

--- app/src/main/java/com/example/magicmamanapplication/ResumeFragment.kt
+++ app/src/main/java/com/example/magicmamanapplication/ResumeFragment.kt
@@ -5,13 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_resume.*
 
 
 class ResumeFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_resume, container, false)
     }
--- app/src/main/java/com/example/magicmamanapplication/Retrofit/Retrofit.kt
+++ app/src/main/java/com/example/magicmamanapplication/Retrofit/Retrofit.kt
@@ -6,7 +6,7 @@ import retrofit2.Retrofit
 
 
 class Retrofit {
-    var BASE_URL = "http://192.168.1.7:3000/" // Localhost will be changed to 10.0.2.2 in Emulator
+    var BASE_URL = "http://192.168.1.16:3000/" // Localhost will be changed to 10.0.2.2 in Emulator
 
     fun getRetroClinetInstance() : Retrofit{
 

00684cf2a24d315a33054db6b09d03eda7dd0d9b
bensalemchaima <chaima.bensalem@esprit.tn>
2021-12-28 16:36:54 +0100

    UpdateChaima

--- app/src/main/java/com/example/magicmamanapplication/activities/Medicaments.kt
+++ app/src/main/java/com/example/magicmamanapplication/activities/Medicaments.kt
@@ -2,11 +2,27 @@ package com.example.magicmamanapplication.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import com.example.magicmamanapplication.R
+import com.example.magicmamanapplication.fragments.TimePickerFragment
 
 class Medicaments : AppCompatActivity() {
+    lateinit var etTimeMedicaments: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_medicaments)
+
+        etTimeMedicaments=findViewById(R.id.etTimeMedicaments)
+        etTimeMedicaments.setOnClickListener{ showTimePickerDialog()}
+    }
+    private fun showTimePickerDialog() {
+        val timePicker = TimePickerFragment {onTimeSelected(it)}
+        timePicker.show(supportFragmentManager, "time")
+    }
+
+    private fun onTimeSelected(time: String){
+        etTimeMedicaments.setText("$time")
+
     }
 }
--- app/src/main/java/com/example/magicmamanapplication/activities/Vaccin.kt
+++ app/src/main/java/com/example/magicmamanapplication/activities/Vaccin.kt
@@ -2,11 +2,27 @@ package com.example.magicmamanapplication.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import com.example.magicmamanapplication.R
+import com.example.magicmamanapplication.fragments.TimePickerFragment
 
 class Vaccin : AppCompatActivity() {
+    lateinit var etTimeVaccin: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_vaccin)
+
+        etTimeVaccin=findViewById(R.id.etTimeVaccin)
+        etTimeVaccin.setOnClickListener{ showTimePickerDialog()}
+    }
+    private fun showTimePickerDialog() {
+        val timePicker = TimePickerFragment {onTimeSelected(it)}
+        timePicker.show(supportFragmentManager, "time")
+    }
+
+    private fun onTimeSelected(time: String){
+        etTimeVaccin.setText("$time")
+
     }
 }

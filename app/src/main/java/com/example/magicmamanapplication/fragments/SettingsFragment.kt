7dd23203b615e10e57d23563db3668716898a6cb
benothmenwalid <walid.benothmen@esprit.tn>
2021-12-29 22:21:15 +0100

    updateWalid

--- app/src/main/java/com/example/magicmamanapplication/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/magicmamanapplication/fragments/SettingsFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.cardview.widget.CardView
 import com.example.magicmamanapplication.R
+import com.example.magicmamanapplication.activities.MainActivity2
 import com.example.magicmamanapplication.activities.ReminderActivity
 import com.example.magicmamanapplication.databinding.SignupTabFragmentBinding
 import kotlinx.android.synthetic.main.fragment_settings.*
@@ -22,7 +23,7 @@ class SettingsFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_settings, container, false)
         reminder_cardv = view.findViewById(R.id.reminder_cardv)
         reminder_cardv.setOnClickListener {
-            val intent = Intent(requireContext(),ReminderActivity::class.java)
+            val intent = Intent(requireContext(),MainActivity2::class.java)
             startActivity(intent)
         }
         return view
